2d6411b4f6544901568cbad72aec2cab245be758
scbz <learning@mail.scbz.cn.eu.org>
2025-08-06 11:40:08 +1000

    Day 02: make up notes of day 01 and finish 11-13

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -10,7 +10,30 @@ struct PositiveNonzeroInteger(u64);
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
         // TODO: This function shouldn't always return an `Ok`.
-        Ok(Self(value as u64))
+        // Using `if`
+        // if value > 0 {
+        //     Ok(PositiveNonzeroInteger(value as u64))
+        // } else if value == 0 {
+        //     Err(CreationError::Zero)
+        // } else {
+        //     Err(CreationError::Negative)
+        // }
+
+        // Or 
+        // by chatgpt.com
+        // match value {
+        //     n if n > 0 => Ok(PositiveNonzeroInteger(n as u64)),
+        //     0 => Err(CreationError::Zero),
+        //     _ => Err(CreationError::Negative),
+        // }
+
+        // From sol
+        use std::cmp::Ordering;
+        match value.cmp(&0) {
+            Ordering::Less => Err(CreationError::Negative),
+            Ordering::Equal => Err(CreationError::Zero),
+            Ordering::Greater => Ok(Self(value as u64)),
+        }
     }
 }
 
